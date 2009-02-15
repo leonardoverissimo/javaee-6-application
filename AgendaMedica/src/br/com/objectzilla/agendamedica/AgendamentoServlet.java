6728e2a97000cc2db44bdc54930ec45e8c989d6f
Leonardo Verissimo da Silva <leonardoveri2101@yahoo.com.br>
2009-02-15 15:29:22 -0300

    Preenchendo o servlet

--- AgendaMedica/src/br/com/objectzilla/agendamedica/AgendamentoServlet.java
+++ AgendaMedica/src/br/com/objectzilla/agendamedica/AgendamentoServlet.java
@@ -1,7 +1,10 @@
 package br.com.objectzilla.agendamedica;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +13,39 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(value="/agendamento")
 public class AgendamentoServlet extends HttpServlet {
+	
+	@EJB
+	private Agendamento agendamento;
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 			IOException {
+		
+		String nomeMedico = req.getParameter("medico");
+		String nomePaciente = req.getParameter("paciente");
+		int hora = Integer.parseInt(req.getParameter("hora"));
+		
+		// criando médico
+		Medico medico = new Medico();
+		medico.setNome(nomeMedico);
+		
+		// criando paciente
+		Paciente paciente = new Paciente();
+		paciente.setNome(nomePaciente);
+		
+		// criando horário
+		Calendar horario = Calendar.getInstance();
+		horario.set(Calendar.HOUR, hora);
+		horario.set(Calendar.MINUTE, 0);
+		horario.set(Calendar.SECOND, 0);
+		
+		// chamando método de agendamento
+		agendamento.marcaConsulta(medico, paciente, horario);
+		
+		PrintWriter pw = resp.getWriter();
+		pw.format("Eu juro que marquei consulta para o paciente %s, com o médico %s, na hora %d.",
+				paciente.getNome(), medico.getNome(), horario.get(Calendar.HOUR));
+		
 		
 	}
 }
